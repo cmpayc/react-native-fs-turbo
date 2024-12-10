284e12088a17b4d5465d9521c7b1a8661ea49d7a
Sergei Kazakov <kazakov.s.fl@gmail.com>
2024-12-10 21:24:53 +0400

    feat: add cleanPath helper

--- cpp/filesystem/helpers.h
+++ cpp/filesystem/helpers.h
@@ -9,6 +9,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <regex>
 #include <vector>
 
 std::string readFile(const char* filePath, int offset, int length);
@@ -20,3 +21,5 @@ void writeFile(const char* filePath, const char* content, bool isAppend);
 void writeFileUint8(const char* filePath, const uint8_t *contentUint8, size_t length, bool isAppend);
 
 void writeWithOffset(const char* filePath, std::string content, int offset);
+
+std::string cleanPath(std::string filePath);
